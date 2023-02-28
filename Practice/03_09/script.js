007831fc6efda3f83af3ec36fe9e558b3327fd2e
CalifieKL <califiekluo@gmail.com>
2023-02-28 00:25:39 -0500

    adds functions to backpack object; calls them in browser

--- Practice/03_09/script.js
+++ Practice/03_09/script.js
@@ -19,8 +19,43 @@ const backpack = {
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
+  changeName: function (newName) {
+    this.name = newName;
+  },
+  modifyVolume: function (newVolume) {
+    this.volume = newVolume;
+  },
+  changeColor: function (newColor) {
+    this.color = newColor;
+  },
+  modifyPocketNum: function (newPocketNum) {
+    this.pocketNum = newPocketNum;
+  },
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
   },
+  getDescription: function () {
+    return JSON.stringify(this);
+  },
 };
+function updateName() {
+  backpack.changeName(document.getElementById("packetName").value);
+  alert("name is now " + backpack.name.toString());
+  console.log(backpack.getDescription());
+}
+function updateVolume() {
+  backpack.modifyVolume(document.getElementById("packetVolume").value);
+  alert("volume is now " + backpack.volume.toString());
+  console.log(backpack.getDescription());
+}
+function updateColor() {
+  backpack.changeColor(document.getElementById("packetColor").value);
+  alert("color is now " + backpack.color.toString());
+  console.log(backpack.getDescription());
+}
+function updatePocketNum() {
+  backpack.modifyPocketNum(document.getElementById("packetPocketNum").value);
+  alert("pocket number is now " + backpack.pocketNum.toString());
+  console.log(backpack.getDescription());
+}
